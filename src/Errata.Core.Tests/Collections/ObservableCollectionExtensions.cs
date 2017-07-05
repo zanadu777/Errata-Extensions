7ca168b5c834ecc24eb63d82be005fca1d6febc0
zanadu <kenjohnson777@hotmail.com>
2017-07-05 01:12:31 -0400

    Fluent assertion version update

--- src/Errata.Core.Tests/Collections/ObservableCollectionExtensions.cs
+++ src/Errata.Core.Tests/Collections/ObservableCollectionExtensions.cs
@@ -6,8 +6,6 @@ namespace Errata.Collections
     public static class ObservableCollectionExtensions
     {
 
-
-
         public static void ReplaceContents<T>(this ObservableCollection<T> collection, IEnumerable<T> items)
         {
             collection.Clear();
